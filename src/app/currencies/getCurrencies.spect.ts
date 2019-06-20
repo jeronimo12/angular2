f4d73365ff91888fb1097836b21e30c02f6b6209
jeronimo12 <156w0218@itszongolica.deu.mx>
2019-06-20 13:28:35 -0500

    currencies feature

--- src/app/currencies/getCurrencies.spect.ts
+++ src/app/currencies/getCurrencies.spect.ts
@@ -1,6 +1,6 @@
-import { getCurrencies } from './ getCurrencies '; 
-describe ( 'currencies ', () => { // currencies suite
- it ( 'should return the supported currencies ', () => { 
+import { getCurrencies } from './getCurrencies '; 
+describe( 'currencies ', () => { // currencies suite
+ it ('should return the supported currencies ', () => { 
 const result = getCurrencies(); 
 expect( result ).toContain( 'USD' ); 
 expect( result ).toContain( 'AUD' );
